4a912d5026b126b52990f45c400d3bc704889c1d
arther606 <396986654@qq.com>
2024-08-13 14:54:51 +0800

    编程式端点

---
+++ src/main/java/com/yang/websocket/chapter1/ProgrammaticEchoServer.java
@@ -0,0 +1,31 @@
+package com.yang.websocket.chapter1;
+
+import java.io.IOException;
+import javax.websocket.Endpoint;
+import javax.websocket.EndpointConfig;
+import javax.websocket.MessageHandler;
+import javax.websocket.Session;
+
+/**
+ * @author chuang
+ */
+public class ProgrammaticEchoServer extends Endpoint {
+
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+        final Session mySession = session;
+        mySession.addMessageHandler(new MessageHandler.Whole<String>() {
+            @Override
+            public void onMessage(String text) {
+                try {
+                    mySession.getBasicRemote().sendText("I got this (" + text + ") so I am sending it back !");
+                } catch (IOException ioe) {
+                    System.out.println("oh dear, something went wrong trying to send the message back: " + ioe.getMessage());
+                }
+            }
+        });
+    }
+
+}
+
+
---
+++ src/main/java/com/yang/websocket/chapter1/ProgrammaticEchoServerAppConfig.java
@@ -0,0 +1,27 @@
+package com.yang.websocket.chapter1;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.websocket.Endpoint;
+import javax.websocket.server.*;
+
+/**
+ * @author chuang
+ */
+public class ProgrammaticEchoServerAppConfig implements ServerApplicationConfig {
+
+    @Override
+    public Set<ServerEndpointConfig> getEndpointConfigs(Set<Class<? extends Endpoint>> endpointClasses) {
+        Set configs = new HashSet<ServerEndpointConfig>();
+        ServerEndpointConfig sec = ServerEndpointConfig.Builder
+                .create(ProgrammaticEchoServer.class, "/programmaticecho")
+                .build();
+        configs.add(sec);
+        return configs;
+    }
+
+    @Override
+    public Set<Class<?>> getAnnotatedEndpointClasses(Set<Class<?>> scanned) {
+        return scanned;
+    }
+}
